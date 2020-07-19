c9a9140db5112e74030763292dc93de25adb3482
robashton <robashton@codeofrob.com>
2020-07-19 19:27:36 +0100

    stick erlang_ls here for now

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ self: super:
       spago-0-12-1-0 = super.callPackage ./support/spago/0.12.1.0.nix {};
       dhall-json-1-5-0 = super.callPackage ./support/dhall/dhall-json-1-5-0.nix {};
       purescript-0-13-6 = super.callPackage ./support/purescript/0.13.6.nix {};
+      erlang_ls-0-4-1 =  super.beam.packages.erlang.callPackage ./support/erlang_ls/0.4.1.nix {};
     };
 }
 
